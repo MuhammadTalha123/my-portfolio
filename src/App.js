ed044a74fafc7ace178ea57964b0bcfea2ff187a
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-01-21 22:32:33 +0500

    update websites section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import Websites from "./Websites";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Websites />
     </div>
   );
 }
---
+++ src/Websites.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function Websites() {
+  return (
+    <div className="Websites">
+      <h1>Featured Work</h1>
+      <div className="first_website">
+         <h2></h2>
+      </div>
+    </div>
+  );
+}
+
+export default Websites;
