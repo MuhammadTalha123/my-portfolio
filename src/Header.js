7ad688d0f595dd6625687461f700b68229d6da1d
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-01-30 23:16:55 +0500

    updtae responsiveness to 500px

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
       <h1>Successful Front-end Development</h1>
-      <p>
+      <p className="des_paragraph">
         Hi. I’m Talha, a freelance Front-end Developer with 2 years <br />
         commercial experience creating successful websites.
       </p>
@@ -15,8 +15,8 @@ function Header() {
             src="https://ianlunn.co.uk/static/0fe210187ee16bce95bb97c7ab3db41b/98c70/feature__web.webp"
             width="150"
           />
-          <h2>Front-end development</h2>
-          <p>
+          <h2 className="img_heading">Front-end development</h2>
+          <p className="img_paragraph">
             Responsive websites built for an optimal user experience that
             achieves your business goals.
           </p>
@@ -26,10 +26,10 @@ function Header() {
             src="https://ianlunn.co.uk/static/54f14637ec376524313424344767614c/98c70/feature__tools.webp"
             width="150"
           />
-          <h2>Performance, SEO, & accessibility testing</h2>
-          <p>
+          <h2 className="img_heading">Performance, SEO, & accessibility testing</h2>
+          <p className="img_paragraph">
             Make your website fast, easy to find, and reach the widest audience
-            possible.
+            possible.  
           </p>
         </div>
       </div>
